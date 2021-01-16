70ffe0d49e81702c7120bc458674e89a9b0c69b2
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2021-01-16 09:58:48 -0800

    Implement pymongo, 404 page, and login page

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask, request, render_template
+from pymongo import MongoClient
+mongoclient = MongoClient()
+db = mongoclient.symptomspace_database
 app = Flask(__name__)
 
 @app.route("/")
 def serve_index():
     return render_template('index.html')
+
+@app.route("/login", methods = ["POST", "GET"])
+def serve_login():
+    if request.method == "GET":
+        return render_template('login.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
    app.run()
