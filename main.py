724a0fecd2b67a9ac4db8714385d0c90d15f8d7c
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2021-01-16 09:30:11 -0800

    Add basic routing and more templates

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from flask import flask
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def serve_index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run()
