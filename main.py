b2a69052b519935c304b951e3652d4c8f828f75a
ian-chuang <iantc104@gmail.com>
2021-01-16 20:14:01 -0800

    styled user_info.html

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def serve_index():
-    return render_template('index.html')
+    return render_template('user_info.html')
 
 @app.route("/login", methods = ["POST", "GET"])
 def serve_login():
