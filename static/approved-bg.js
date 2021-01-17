410f544aa061d1f3225d35ad8634c79a748c3a6b
sarahayu <46186848+sarahayu@users.noreply.github.com>
2021-01-17 12:18:30 -0800

    auto submit and made confirm button brighter

--- static/approved-bg.js
+++ static/approved-bg.js
@@ -1,16 +1,24 @@
-const possible = [
-    'https://i.imgur.com/UEW1hwx.jpg',
-    'https://i.imgur.com/fM3Av8a.jpg',
-    'https://i.imgur.com/hUWO3ZY.jpg',
-    'https://i.imgur.com/K8JyjXW.jpg',
-    'https://i.imgur.com/mVBYiPv.jpg',
-    'https://i.imgur.com/SRyM8UH.jpg',
-    'https://i.imgur.com/iCJt8de.jpg',
-    'https://i.imgur.com/03xDjcY.jpg',
-    'https://i.imgur.com/id2wmS4.jpg',
-    'https://i.imgur.com/ukyPAJC.jpg',
-    'https://i.imgur.com/m0PcIDl.jpg',
-    'https://i.imgur.com/Pbt42Bi.jpg',
-    'https://i.imgur.com/YUpXCQp.jpg'
-]
-document.querySelector('.centered-parent').style.backgroundImage = `url('${possible[Math.floor(Math.random() * possible.length)]}')`
+(function()
+{
+    const possible = [
+        'https://i.imgur.com/UEW1hwx.jpg',
+        'https://i.imgur.com/fM3Av8a.jpg',
+        'https://i.imgur.com/hUWO3ZY.jpg',
+        'https://i.imgur.com/K8JyjXW.jpg',
+        'https://i.imgur.com/mVBYiPv.jpg',
+        'https://i.imgur.com/SRyM8UH.jpg',
+        'https://i.imgur.com/iCJt8de.jpg',
+        'https://i.imgur.com/03xDjcY.jpg',
+        'https://i.imgur.com/id2wmS4.jpg',
+        'https://i.imgur.com/ukyPAJC.jpg',
+        'https://i.imgur.com/m0PcIDl.jpg',
+        'https://i.imgur.com/Pbt42Bi.jpg',
+        'https://i.imgur.com/YUpXCQp.jpg'
+    ]
+    const bgContainer = document.querySelector('.centered-parent');
+    bgContainer.style.backgroundImage = `url('${possible[Math.floor(Math.random() * possible.length)]}')`
+    bgContainer.style.backgroundSize = 'cover';
+    bgContainer.style.backgroundPosition = 'center';
+    bgContainer.style.width = '100%';
+    bgContainer.style.minHeight = '100%';
+})()
